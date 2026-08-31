76e0883016051d470e6c4892adc6573cea7f2ca1
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-08-31 13:55:42 +0530

    31082026 connector chcking v4 1355

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 
 const fs = require("fs");
+const path = require("path");
+
+const LOG_DIR =
+  path.join(__dirname, "logs");
+
+if (!fs.existsSync(LOG_DIR)) {
+
+  fs.mkdirSync(LOG_DIR, {
+    recursive: true
+  });
+
+}
 
 function masterFlowDebug(data){
 
@@ -39,7 +51,6 @@ function syncDebug(data){
 
 }
 
-const path = require("path");
 
 const nodemailer =
   require("nodemailer");
